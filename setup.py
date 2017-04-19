294e5ee827f2f7a9d0ba5e67b01401b59971129a
Umair Ashraf <umrashrf@gmail.com>
2017-04-19 13:01:30 -0400

    make it an official python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nabcommerce',
+    version='1.0.0',
+    url='https://nabvelocity.com/',
+    description='Python wrapper for NABVelocity\'s REST API',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    zip_safe=False
+)
